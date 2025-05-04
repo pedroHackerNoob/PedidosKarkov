ee4b57dfe75136b80309db37506c0a1f5c36c680
Tyler Brock <pedrohardbass@gmail.com>
2025-05-04 01:52:41 -0700

    ServiceSortPriority updated

--- src/karkov/services/ServiceSortPriority.java
+++ src/karkov/services/ServiceSortPriority.java
@@ -11,14 +11,21 @@ public class ServiceSortPriority {
 
     public static void sortByPriority() {
         ArrayList<Request> requestsSortLow = new ArrayList<>();
-        ArrayList<Request> requestsSortMiddle = new ArrayList<>();
+        ArrayList<Request> requestsSortMedium = new ArrayList<>();
         ArrayList<Request> requestsSortHigh = new ArrayList<>();
 
         for(int i = 0; i < RepositoryRequest.getRequests().size(); i++){
-            if (RepositoryRequest.getRequests().get(i).getPrority().equals("baja")){
+            if (RepositoryRequest.getRequests().get(i).getPrority().equals("baja")) {
                 requestsSortLow.add(RepositoryRequest.getRequests().get(i));
+            } else if (RepositoryRequest.getRequests().get(i).getPrority().equals("media")) {
+                requestsSortMedium.add(RepositoryRequest.getRequests().get(i));
+            } else if (RepositoryRequest.getRequests().get(i).getPrority().equals("alta")) {
+                requestsSortHigh.add(RepositoryRequest.getRequests().get(i));
             }
+
         }
+        requestsSort.addAll(requestsSortHigh);
+        requestsSort.addAll(requestsSortMedium);
         requestsSort.addAll(requestsSortLow);
 
     }
